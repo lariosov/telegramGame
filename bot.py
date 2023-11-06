b39b988c75b8746851b5bec131a7945737aa3b1b
Anton Levitskiy <larios18000@gmail.com>
2023-11-06 13:34:48 +0300

    .env enable

--- bot.py
+++ bot.py
@@ -7,22 +7,26 @@ import os
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 # Константы внутри бота
-TOKEN = os.getenv('TOKEN')
-bot = Bot(TOKEN)
+BOT_TOKEN = os.getenv('TOKEN')
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
 def main():
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=False)
 
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
     text = const_text.INTRO_TEXT
-    await message.answer(f'Привет, {message.from_user.full_name}!', text)
+    await message.answer(f'Привет, {message.from_user.full_name}! {text}')
 
 
 # Чел... это бестпрактис (нет)
