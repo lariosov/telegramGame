2a4617f6811d43ec454b8712dfe5241eb735ac4e
Anton Levitskiy <larios18000@gmail.com>
2023-11-06 14:19:36 +0300

    first db created

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # Основные импорты
 import const_text
+import sqlite3
 import aiogram
 import os
 
@@ -10,7 +11,48 @@ from aiogram.utils import executor
 from dotenv import load_dotenv
 
 
+# Подгружаем енв переменные
 load_dotenv()
+
+
+# Настройки БД
+con = sqlite3.connect('db.sqlite', check_same_threads = False)
+cur = con.cursor()
+
+
+# Создаем БД
+def start_database():
+    # Создаем таблицу пользователей
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        NAME TEXT,
+        USER_NAME TEXT NOT NULL,
+        USER_ID TEXT NOT NULL,
+        USER_BALANCE INTEGER NOT NULL
+        );
+    ''')
+    con.commit()
+
+    # Создаем таблицу активностей
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS activity(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        NAME TEXT,
+        ACTIVITY_PRICE INTEGER NOT NULL,
+        );
+    ''')
+    con.commit()
+
+    # Создаем таблицу магазина
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS shop(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        NAME TEXT,
+        SHOP_UNIT_COST INTEGER NOT NULL
+        );
+    ''')
+    con.commit()
 
 
 # Константы внутри бота
@@ -20,7 +62,7 @@ dp = Dispatcher(bot)
 
 
 def main():
-    executor.start_polling(dp, skip_updates=False)
+    executor.start_polling(dp, skip_updates=True)
 
 
 @dp.message_handler(commands='start')
